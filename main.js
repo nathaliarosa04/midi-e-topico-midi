4268ad524a02e6bf41ad6798388d160572fc39ed
nathaliarosa04 <108874082+nathaliarosa04@users.noreply.github.com>
2023-09-20 12:36:06 -0300

    s

--- main.js
+++ main.js
@@ -2,22 +2,33 @@
 function tocaSomPom() {
 document.querySelector('#som_tecla_pom').play();
 }
- 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    
 
+ 
 //tecla clap
-function tocaSomPom() {
+function tocaSomclap() {
 document.querySelector('#som_tecla_clap').play();
 }
  
-document.querySelector('.tecla_clap).onclick = tocaSomClap;
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 //tecla tim
-function tocaSomtim () {
+function tocaSomtim() {
     document.querySelector('#som_tecla_tim').play();
     }
      
-    document.querySelector('.tecla_tim).onclick = tocaSomTim;
+    document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+   //tecla puff 
+   unction tocaSompuff() {
+    document.querySelector('#som_tecla_puff').play();
+    }
+     
+    document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+    
+
+    
 
     
 
