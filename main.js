090af81796c1198ef2261006d19c5ee9f248cc9f
nathaliarosa04 <108874082+nathaliarosa04@users.noreply.github.com>
2023-09-20 12:28:39 -0300

    s

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 //tecla Pom
-function tocaSomPom() {}
+function tocaSomPom() {
 document.querySelector('#som_tecla_pom').play();
 }
  
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 //tecla clap
-function tocaSomPom() {}
+function tocaSomPom() {
 document.querySelector('#som_tecla_clap').play();
 }
  
 document.querySelector('.tecla_clap).onclick = tocaSomClap;
+
+//tecla tim
+function tocaSomtim () {
+    document.querySelector('#som_tecla_tim').play();
+    }
+     
+    document.querySelector('.tecla_tim).onclick = tocaSomTim;
+
+    
 
 
 
