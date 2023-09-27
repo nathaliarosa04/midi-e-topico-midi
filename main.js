17bf0cec5fcc9b6f1ac4f9b7d85ae4193d40c9d1
nathaliarosa04 <108874082+nathaliarosa04@users.noreply.github.com>
2023-09-27 12:13:09 -0300

    s

--- main.js
+++ main.js
@@ -19,12 +19,65 @@ function tocaSomtim() {
      
     document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
-   //tecla puff 
-   unction tocaSompuff() {
+    //tecla puff
+function tocaSompuff() {
     document.querySelector('#som_tecla_puff').play();
     }
      
-    document.querySelector('.tecla_puff').onclick = tocaSompuff;
+    document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+     //tecla splash
+function tocaSomsplash() {
+    document.querySelector('#som_tecla_splash').play();
+    }
+     
+    document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+    //tecla toim
+function tocaSomtoim() {
+    document.querySelector('#som_tecla_toim').play();
+    }
+     
+    document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+    //tecla psh
+function tocaSompsh() {
+    document.querySelector('#som_tecla_psh').play();
+    }
+     
+    document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+    //tecla tic
+function tocaSomtic() {
+    document.querySelector('#som_tecla_tic').play();
+    }
+     
+    document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+    //tecla tom
+function tocaSomtom() {
+    document.querySelector('#som_tecla_tom').play();
+    }
+     
+    document.querySelector('.tecla_tic').onclick = tocaSomTom;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+     
 
     
 
