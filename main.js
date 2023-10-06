c42e62e87150e8b4637550ec2e46334f0fa415df
nathaliarosa04 <108874082+nathaliarosa04@users.noreply.github.com>
2023-10-06 11:59:22 -0300

    s

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function tocaSomtom() {
     document.querySelector('#som_tecla_tom').play();
     }
      
-    document.querySelector('.tecla_tic').onclick = tocaSomTom;
+    document.querySelector('.tecla_tom').onclick = tocaSomTom;
 
 
 
